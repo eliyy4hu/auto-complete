4d1f6b16d7508765b5dc8569fd3eb89ee538b1f8
eliyy4hu <eliyy4hu@outlook.com>
2021-01-12 23:04:10 +0500

    add SimpleDictionaryService

---
+++ Core/Services/IDictionaryUpdaterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Services
+{
+    public interface IDictionaryUpdaterService
+    {
+        public void InitDictionary(IEnumerable<string> words);
+        public void UpdateDictionary(IEnumerable<string> words);
+        public void ClearDictionary();
+    }
+}
---
+++ Core/Utils/FrequencyAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utils
+{
+    public class FrequencyAnalyser
+    {
+        public Dictionary<string,int> GetEntriesCount(IEnumerable<string> source)
+        {
+            var result = new Dictionary<string, int>();
+            foreach (var item in source)
+            {
+                if (result.ContainsKey(item))
+                {
+                    result[item]++;
+                }
+                else
+                {
+                    result[item] = 1;
+                }
+            }
+            return result;
+        }
+    }
+}
